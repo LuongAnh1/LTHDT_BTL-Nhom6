0b4ab7a4cd023b0021660b121f6886800c213b61
nbdh7345-design <nbdh7345@gmail.com>
2025-12-20 23:51:15 +0700

    quan ly thong tin danh muc

--- BTL-Nhom6/Quan_Ly_Thong_Tin_Danh_Muc/QLLTB_va_Model.xaml.cs
+++ BTL-Nhom6/Quan_Ly_Thong_Tin_Danh_Muc/QLLTB_va_Model.xaml.cs
@@ -1,27 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using BTL_Nhom6.Helper; // Đảm bảo đúng namespace của NavigationHelper
 
 namespace BTL_Nhom6.Quan_Ly_Thong_Tin_Danh_Muc
 {
-    /// <summary>
-    /// Interaction logic for QLLTB_va_Model.xaml
-    /// </summary>
     public partial class QLLTB_va_Model : Window
     {
         public QLLTB_va_Model()
         {
             InitializeComponent();
         }
+
+        #region Điều hướng Tab chính
+
+        // Chuyển sang Quản lý vị trí phòng ban
+        private void Button_QLVTPB_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationHelper.Navigate(this, new QLVTPB());
+        }
+
+        // Chuyển sang Nhà cung cấp & Báo giá
+        private void Button_NCC_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationHelper.Navigate(this, new NCC_va_BGLK());
+        }
+
+        // Chuyển sang Từ điển dữ liệu chung (TDDLC.xaml)
+        private void Button_TDDL_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationHelper.Navigate(this, new TDDLC());
+        }
+
+        #endregion
+
+        #region Các hành động khác (Search, Thêm, Sửa, Xóa)
+        // Bạn có thể viết logic lọc dữ liệu cho các ô txtSearch tại đây
+        #endregion
     }
 }
---
+++ BTL-Nhom6/Quan_Ly_Thong_Tin_Danh_Muc/TDDLC2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using BTL_Nhom6.Helper;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace BTL_Nhom6.Quan_Ly_Thong_Tin_Danh_Muc
+{
+    public partial class TDDLC2 : Window
+    {
+        public TDDLC2()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            icDonViTinh.ItemsSource = new List<object> {
+                new { Ma = "CAI", Ten = "Cái", MoTa = "Đơn vị tính đếm được." },
+                new { Ma = "BO", Ten = "Bộ", MoTa = "Tập hợp các vật thể." }
+            };
+        }
+
+        #region Chuyển đổi Danh mục lớn
+        private void Button_QLVTPB_Click(object sender, RoutedEventArgs e) => NavigationHelper.Navigate(this, new QLVTPB());
+        private void Button_QLLTB_Click(object sender, RoutedEventArgs e) => NavigationHelper.Navigate(this, new QLLTB_va_Model());
+        private void Button_NCC_Click(object sender, RoutedEventArgs e) => NavigationHelper.Navigate(this, new NCC_va_BGLK());
+        #endregion
+
+        #region Chuyển đổi Tab con
+        private void Button_TrangThai_Click(object sender, RoutedEventArgs e) =>
+            NavigationHelper.Navigate(this, new TDDLC()); // Về trang 1
+
+        private void Button_Loi_Click(object sender, RoutedEventArgs e) =>
+            NavigationHelper.Navigate(this, new TDDLC3()); // Sang trang 3
+        #endregion
+    }
+}
