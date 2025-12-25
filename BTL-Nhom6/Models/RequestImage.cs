d58c24c908ad18b7695c5a5d380cc2144da9d0a9
LuongAnh1 <93256715+LuongAnh1@users.noreply.github.com>
2025-12-25 11:24:35 +0700

    Xong QLYCBT

---
+++ BTL-Nhom6/Models/RequestImage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BTL_Nhom6.Models
+{
+    public class RequestImage
+    {
+        public int ImageID { get; set; }
+        public int RequestID { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime UploadedAt { get; set; }
+    }
+}
